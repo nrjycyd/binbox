d551d7e5ecfd3221a46068daa588381b83a32253
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2025-12-09 23:16:53 +0800

    Update fetch-binaries.sh

--- .github/workflows/fetch-binaries.sh
+++ .github/workflows/fetch-binaries.sh
@@ -63,7 +63,6 @@ for ((i=0; i<count; i++)); do
         if [[ "$ft" == "zip" ]]; then unzip -qo "$pkgfile" -d "$extract_tmp"; fi
         if [[ "$ft" == "tar.gz" ]]; then tar -xzf "$pkgfile" -C "$extract_tmp"; fi
         if [[ "$ft" == "gz" ]]; then gunzip -c "$pkgfile" > "$extract_tmp/$(basename "$pkgfile" .gz)"; fi
-        if [[ "$ft" == "gz" ]]; then gunzip -c "$pkgfile" > "$extract_tmp/$(basename "$pkgfile" .gz)"; fi
 
         # 平铺文件到临时目录
         shopt -s dotglob
