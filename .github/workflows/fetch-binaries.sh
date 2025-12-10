bb4aba5ee15bda13305c9df0e8100317f8116861
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2025-12-10 11:00:16 +0800

    Update fetch-binaries.sh

--- .github/workflows/fetch-binaries.sh
+++ .github/workflows/fetch-binaries.sh
@@ -76,6 +76,7 @@ for ((i=0; i<count; i++)); do
         
         if [[ "$ft" == "zip" ]]; then unzip -qo "$pkgfile" -d "$extract_tmp"; fi
         if [[ "$ft" == "tar.gz" ]]; then tar -xzf "$pkgfile" -C "$extract_tmp"; fi
+        if [[ "$ft" == "tar.xz" ]]; then tar -xJf "$pkgfile" -C "$extract_tmp"; fi
         if [[ "$ft" == "gz" ]]; then gunzip -c "$pkgfile" > "$extract_tmp/$(basename "$pkgfile" .gz)" && chmod +x "$extract_tmp/$(basename "$pkgfile" .gz)"; fi
 
         # 平铺文件到临时目录
